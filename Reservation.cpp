34c60488e833d0d19292baf25f05a8bb4274f759
Youssef_H <148619505+Youssef-Hatem-S@users.noreply.github.com>
2025-09-13 02:56:02 +0300

    Delete Reservation.cpp

--- Reservation.cpp
+++
@@ -1,9 +0,0 @@
-#include "Reservation.h"
-
-Reservation::Reservation(int id, int gId, const std::string& gName,
-                         const std::string& ph, int rNum,
-                         const std::string& in, const std::string& out,
-                         const std::string& hotel)
-        : id(id), guestId(gId), guestName(gName), phone(ph),
-          roomNumber(rNum), checkInDate(in), checkOutDate(out),
-          hotelName(hotel) {}
